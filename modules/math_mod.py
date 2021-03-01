388a772f5f518e14ff09ed6775d5cb03c79d9c78
xalhs <tagarism@gmail.com>
2021-03-01 07:04:47 +0200

    Add headers to request

--- modules/math_mod.py
+++ modules/math_mod.py
@@ -3,7 +3,7 @@ import json
 
 def math(expression):
     body = json.dumps({"expr": expression})
-    request = requests.post("http://api.mathjs.org/v4/" , data = body ).json()
+    request = requests.post("http://api.mathjs.org/v4/" , headers = {"content-type": "application/json"} , data = body ).json()
     print(request)
     if request["error"] == None:
         return(request["result"])
@@ -12,5 +12,5 @@ def math(expression):
 
 nam ="3^2 + 4^2"
 body = json.dumps({"expr": nam})
-request = requests.post("http://api.mathjs.org/v4/" , data = body ).json()
+request = requests.post("http://api.mathjs.org/v4/" , headers = {"content-type": "application/json"} , data = body ).json()
 print(request["result"])
